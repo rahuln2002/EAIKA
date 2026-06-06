05e717ab6c1991bde33610b7c943ba0ffaea9b1f
Rahul Nihalani <itsnrahul@gmail.com>
2026-06-06 19:52:10 +0530

    Test: 2

--- backend/start.sh
+++ backend/start.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
+set -x
+
 python - <<'EOF'
-print("1")
-from app.api.middleware import setup_auth_middleware
-print("2")
-from app.core.config import settings
-print("3")
-from app.exceptions import register_exception_handlers
-print("4")
-from app.monitoring.prometheus import router
-print("5")
-from app.monitoring.healthcheck import healthcheck
-print("6")
-from app.api.router import api_router
-print("7")
+print("START")
+
+import app.api.router
+
+print("ROUTER IMPORTED")
+EOF
 
 echo "Waiting for PostgreSQL..."
 
