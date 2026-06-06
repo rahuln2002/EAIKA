588c810c125e50dc1a86c09abaea6d826b6e83f4
Rahul Nihalani <itsnrahul@gmail.com>
2026-06-06 19:41:21 +0530

    Test: 2

--- backend/start.sh
+++ backend/start.sh
@@ -1,19 +1,33 @@
 #!/bin/sh
 
-python -c "import app.main; print('IMPORT SUCCESS')"
+python - <<'EOF'
+print("1")
+from app.api.middleware import setup_auth_middleware
+print("2")
+from app.core.config import settings
+print("3")
+from app.exceptions import register_exception_handlers
+print("4")
+from app.monitoring.prometheus import router
+print("5")
+from app.monitoring.healthcheck import healthcheck
+print("6")
+from app.api.router import api_router
+print("7")
+EOF
 
 echo "Waiting for PostgreSQL..."
 
 sleep 15
 
-# echo "Running Alembic migrations..."
+echo "Running Alembic migrations..."
 
-# alembic upgrade head
+alembic upgrade head
 
 echo "Starting FastAPI..."
 echo "PORT=$PORT"
 
 uvicorn app.main:app \
 	--host 0.0.0.0 \
-	--port ${PORT}
+	--port ${PORT} \
 	--log-level debug
